1db3e18ef6328abe1875d20cbfb03a62b1b5057e
27k <mail@udesh.xyz>
2022-06-16 22:18:50 +0530

    Initialize console project

---
+++ console/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      OpenXDR
+    </div>
+  );
+}
+
+export default App;
