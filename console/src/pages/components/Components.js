129103ff1349433197ef5e5491421ec95147d6d3
UsefulAd1242 <upalanka.k@gmail.com>
2022-07-05 13:36:04 +0530

    Implemented Log Collection component section

--- console/src/pages/components/Components.js
+++ console/src/pages/components/Components.js
@@ -1,13 +1,18 @@
-import React, {  } from 'react';
+import React, { } from 'react';
 
 import './Components.css';
 import PageToolbar from '../../components/PageToolbar';
+import ComponentList from './ComponentList';
+import { Container } from '@mui/material';
 
 function Components() {
 
   return (
     <div className="Components">
-      <PageToolbar title="Components"/>
+      <PageToolbar title="Components" />
+      <Container maxWidth="lg" sx={{ mt: 6, mb: 4 }}>
+        <ComponentList />
+      </Container>
     </div>
   );
 }
--- console/src/theme.js
+++ console/src/theme.js
@@ -3,14 +3,26 @@ export const defaultTheme = {
         primary: {
             main: "#222",
         },
+        secondary: {
+            main: '#304ffe',
+        },
+        success: {
+            main: '#9ccc65',
+            contrastText: '#fff',
+        },
+        error: {
+            main: '#ef5350',
+        },
     },
     shape: {
         borderRadius: 12,
     },
-    overrides: {
+    components: {
         MuiButton: {
-            root: {
-                borderRadius: 4,
+            styleOverrides: {
+                root: {
+                    borderRadius: 6,
+                },
             },
         },
     },
