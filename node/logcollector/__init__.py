f9ece3b5598321765f7b904585f56201c5e39dcc
Piumal Kulasekara <piumal.kulasekara@gmail.com>
2022-07-16 21:06:05 -0700

    add init import

---
+++ node/logcollector/__init__.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+
+def logtreem(websocket, filename):
+    file = open(filename, 'r')
+    st_results = os.stat(filename)
+    st_size = st_results[6]
+    file.seek(st_size)
+
+    while 1:
+        where = file.tell()
+        line = file.readline()
+        if not line:
+            time.sleep(1)
+            file.seek(where)
+        else:
+            await websocket.send(line)
+
