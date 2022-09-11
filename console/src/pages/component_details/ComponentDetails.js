c99a074aff126c62b1c72724b190e635ceeba9d4
UsefulAd1242 <upalanka.k@gmail.com>
2022-09-11 10:32:12 +0530

    Implemented log management

--- console/src/pages/component_details/ComponentDetails.js
+++ console/src/pages/component_details/ComponentDetails.js
@@ -1,13 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useForm } from "react-hook-form";
 
 import './ComponentDetails.css';
 import { Box, Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
 
-function ComponentDetails() {
-  const { os, setOs } = useState(0);
+import * as ConsoleApi from '../../services';
 
-  const handleChange = (event) => {
-    setOs(event.target.value);
+function ComponentDetails({data, onComplete}) {
+  const { register, handleSubmit, setValue, formState: { errors } } = useForm();
+
+  useEffect(() => {
+    if (data !== null) {
+      setValue('name', data.name)
+      setValue('os', data.os)
+      setValue('source', data.source)
+    }
+  }, [data]);
+
+  const onCreate = (data) => {
+    console.log("Create")
+    var api = new ConsoleApi.DefaultApi();
+    var requestData = ConsoleApi.LogsCreateRequest.constructFromObject(data);
+    var callback = function(error, data, response) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log('API called successfully. Returned data: ' + data);
+        console.log(data)
+      }
+      onComplete(false);
+    };
+    api.create({'logsCreateRequest': requestData}, callback);
+  };
+
+  const onUpdate = (newData) => {
+    console.log("Update")
+    var api = new ConsoleApi.DefaultApi();
+    var requestData = data;
+    requestData.name = newData.name;
+    requestData.os = newData.os;
+    requestData.source = newData.source;
+    var callback = function(error, data, response) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log('API called successfully. Returned data: ' + data);
+        console.log(data)
+      }
+      onComplete(false);
+    };
+    api.update(data.id, {'log': requestData}, callback);
   };
 
   return (
@@ -25,20 +67,19 @@ function ComponentDetails() {
           label="Component Name"
           placeholder="Linux Log"
           variant="standard"
+          {...register("name")}
         />
         <div>
           <FormControl required variant="standard" sx={{ mb: 2, minWidth: 240 }}>
-            <InputLabel id="demo-simple-select-label">Operating System</InputLabel>
+            <InputLabel>Operating System</InputLabel>
             <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={os}
               label="Operating System"
-              onChange={handleChange}
+              defaultValue={'linux'}
+              {...register("os", { required: true })}
             >
-              <MenuItem value={0}>Linux</MenuItem>
-              <MenuItem value={1}>Windows</MenuItem>
-              <MenuItem value={2}>MacOS</MenuItem>
+              <MenuItem value={'linux'}>Linux</MenuItem>
+              <MenuItem value={'win'}>Windows</MenuItem>
+              <MenuItem value={'macos'}>MacOS</MenuItem>
             </Select>
           </FormControl>
         </div>
@@ -49,11 +90,14 @@ function ComponentDetails() {
           label="Source"
           placeholder="/var/log/syslog"
           variant="standard"
+          {...register("source", { required: true })}
         />
         <Box sx={{float: 'right'}}>
-          <Button sx={{ ml: 1, mt: 4 }} variant="contained" disableElevation>
-            ADD
-          </Button>
+        {data !== null ?
+        <Button sx={{ ml: 1, mt: 4 }} variant="contained" disableElevation onClick={handleSubmit(onUpdate)}>UPDATE</Button>:
+        <Button sx={{ ml: 1, mt: 4 }} variant="contained" disableElevation onClick={handleSubmit(onCreate)}>CREATE
+        </Button>
+        }
         </Box>
       </div>
     </Box>
